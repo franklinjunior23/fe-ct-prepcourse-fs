161f3fbeec0e4b7d2dffa5d84c8246e4c30809de
Franx Junior <franklinjunior021118@gmail.com>
2023-06-27 11:52:09 -0500

    primer modulo de jaavascript 03/ terminado

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,36 +5,48 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   $valor = x == y ? true :false;
+   return $valor
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   $valor = str1.length == str2.length ? true : false
+   return $valor
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
-}
+   $valor = num <90 ? true:false
+   return $valor
+}  
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
+   $valor = num >50 ? true : false
+   return $valor
 }
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   $valor = num %2 == 0 ? true : false
+   return $valor
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   $valor = num %2 !== 0 ? true : false
+   return $valor
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
